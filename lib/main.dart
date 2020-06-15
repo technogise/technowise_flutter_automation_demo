e934904d0af1d12849b677b6328a00ec56308a65
atmaram <atmaram@Atmaram-Macbook.local>
2020-06-15 21:23:30 +0530

    ATM | app along with multiple launch screens

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:persona/view/HomePage.dart';
+
+import 'app.dart';
+void main() => runApp(PersonaApp('prod',HomePage(title: 'List of Persons')));
+
+
---
+++ lib/personalist_main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'app.dart';
+import 'view/PersonListPage.dart';
+void main() => runApp(PersonaApp('prod',PersonListPage(title: 'List of Persons')));
+
