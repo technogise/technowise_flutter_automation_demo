6380127ba19ff9e22b2b35ac73f18b41c612462f
atmaram <atmaram@Atmaram-Macbook.local>
2020-06-21 16:04:43 +0530

    ATM | Example with dsl style step definitions

--- test_driver/gherkin_test.dart
+++ test_driver/gherkin_test.dart
@@ -1,25 +1,14 @@
 import 'dart:async';
 
-import 'package:flutter_gherkin/flutter_gherkin.dart';
-import 'package:gherkin/gherkin.dart';
-import 'package:glob/glob.dart';
+import 'package:flutter_gherkin_addons/wrapper.dart';
 
 import 'steps/custom_steps.dart';
 
 Future<void> main() {
-  final config = FlutterTestConfiguration()
-    ..features = [Glob(r"test_driver/features/**.feature")]
-    ..reporters = [
-      ProgressReporter(),
-      TestRunSummaryReporter(),
-      FlutterDriverReporter(),
-      JsonReporter(path: 'report.json')
-    ]
-    ..stepDefinitions = [ScrollUntilTextVisible(), VerifyPersonsList()]
-    ..hooks = [AttachScreenshotOnFailedStepHook()]
-    ..restartAppBetweenScenarios = true
-    ..tagExpression = "@run"
-    ..targetAppPath = "test_driver/app.dart"
-    ..exitAfterTestRun = true;
-  return GherkinRunner().execute(config);
+  return TestRuntime.start(
+      [
+        scrollUntillTextVisible(),
+        verifyPersonaList(),
+      ]
+  );
 }
--- test_driver/steps/custom_steps.dart
+++ test_driver/steps/custom_steps.dart
@@ -2,49 +2,31 @@ import 'dart:async';
 
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
+import 'package:flutter_gherkin_addons/flutter_gherkin_addons.dart';
 import 'package:gherkin/gherkin.dart';
 
-class ScrollUntilTextVisible
-    extends When2WithWorld<String, String, FlutterWorld> {
-  ScrollUntilTextVisible()
-      : super(StepDefinitionConfiguration()
-          ..timeout = const Duration(seconds: 30));
-
-  @override
-  RegExp get pattern =>
-      RegExp(r'I scroll {string} down until {string} is visible');
-
-  @override
-  Future<void> executeStep(String scrollableList, String targetPersona) async {
-    await world.driver.scrollUntilVisible(
-        find.byType(scrollableList), find.text(targetPersona),
-        dxScroll: 0.0, dyScroll: -300);
-  }
+GenericWhen2 scrollUntillTextVisible(){
+  return when2(r'I scroll {string} down until {string} is visible', (context,String scrollableList, String targetPersona) async {
+    await context.world.driver.scrollUntilVisible(
+    find.byType(scrollableList), find.text(targetPersona),
+    dxScroll: 0.0, dyScroll: -300.0);
+  });
 }
-
-class VerifyPersonsList extends When1WithWorld<Table, FlutterWorld> {
-  VerifyPersonsList()
-      : super(StepDefinitionConfiguration()
-          ..timeout = const Duration(seconds: 30));
-
-  @override
-  RegExp get pattern =>
-      RegExp(r'the following list of persons should be displayed');
-
-  @override
-  Future<void> executeStep(Table personsTable) async {
+GenericWhen1 verifyPersonaList(){
+  return when1(r'the following list of persons should be displayed', (context,Table personsTable) async {
     final List<Map<String, String>> personsMap = personsTable.asMap();
     for (int i = 0; i < personsMap.length; i++) {
       String person = personsMap[i]['Name'];
       print("===>Checking for $person in the list");
 
-      await world.driver.scrollUntilVisible(
+      await context.world.driver.scrollUntilVisible(
           find.byType('ListView'), find.text(person),
-          dxScroll: 0.0, dyScroll: -300, timeout: const Duration(seconds: 10));
+          dxScroll: 0.0, dyScroll: -300.0, timeout: const Duration(seconds: 20));
 
       bool isPersonDisplayed =
-          await FlutterDriverUtils.isPresent(find.text(person), world.driver);
-      expect(isPersonDisplayed, true);
+      await FlutterDriverUtils.isPresent(
+          find.text(person), context.world.driver);
+      await context.expect(isPersonDisplayed, true);
     }
-  }
+  },StepDefinitionConfiguration()..timeout = Duration(seconds: 60));
 }
