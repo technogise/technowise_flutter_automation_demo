4fb908d1417cb1db5c7386866f5a59016fe6e9f4
atmaram <atmaram@Atmaram-Macbook.local>
2020-06-21 18:45:59 +0530

    ATM | added indentation

--- test_driver/steps/custom_steps.dart
+++ test_driver/steps/custom_steps.dart
@@ -6,14 +6,16 @@ import 'package:flutter_gherkin_addons/flutter_gherkin_addons.dart';
 import 'package:gherkin/gherkin.dart';
 
 GenericWhen2 scrollUntillTextVisible(){
-  return when2(r'I scroll {string} down until {string} is visible', (context,String scrollableList, String targetPersona) async {
+  return when2(r'I scroll {string} down until {string} is visible',
+          (context,String scrollableList, String targetPersona) async {
     await context.world.driver.scrollUntilVisible(
     find.byType(scrollableList), find.text(targetPersona),
     dxScroll: 0.0, dyScroll: -300.0);
   });
 }
 GenericWhen1 verifyPersonaList(){
-  return when1(r'the following list of persons should be displayed', (context,Table personsTable) async {
+  return when1(r'the following list of persons should be displayed',
+          (context,Table personsTable) async {
     final List<Map<String, String>> personsMap = personsTable.asMap();
     for (int i = 0; i < personsMap.length; i++) {
       String person = personsMap[i]['Name'];
